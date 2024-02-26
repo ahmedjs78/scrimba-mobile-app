52a08a1e51695c7b0a136bd5cc0e96b97c7e1435
ahmedjs78 <ahmedjs778@gmail.com>
2024-02-26 23:43:17 +0400

    working

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref,push, onValue} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {getDatabase, ref,push, onValue,remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 
 const appSettings = {
     databaseURL: "https://realtime-database-b4c14-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
-
+let bomba = []
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "books")
@@ -21,47 +21,63 @@ addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     if(inputValue.trim() === "" ){
         inputFieldEl.placeholder = "Please enter a value";
+        shopingEl.innerHTML = ""
         renderItems(bomba)
     }else{
+        shopingEl.innerHTML = ""
         renderItems(bomba)
         push(shoppingListInDB,inputValue)
     }
     
     
 })
-let bomba 
+
 onValue(shoppingListInDB, function(snapshot){
-    clearUl()
+    
     let turnSnapShotDataToArry = Object.entries(snapshot.val())
     console.log(Array.isArray(turnSnapShotDataToArry))
 
-    let currentValue
+    
     for(let i =0 ; i < turnSnapShotDataToArry.length; i++){
     
     let currentitem = turnSnapShotDataToArry[i]
-    console.log(currentitem)
     let currentId = currentitem[0]
-    currentValue = currentitem[1]
-    console.log(currentValue)
-
+    let currentValue = currentitem[1]
+    bomba.push(currentitem)
     }
 
 
-
-    bomba = currentValue
-    
-    renderItems()
+    renderItems(bomba)
 })
 
-function clearUl(){
-    shopingEl.innerHTML = ""
+function clearUl() {
+        shopingEl.innerHTML = "";
+   
 }
 
 function renderItems(x){
-    let listItems = ""
-    for(let i = 0;i < 8;i++){
-        listItems += `<li>${x}</li>`
-    }
+    console.log('called')
+    for(let i = 0;i < x.length;i++){
+        let currentItem = x[i]
+        let itemId = currentItem[0]
+        let itemValue = currentItem[1]
+
+        let newEl = document.createElement("li")
+        newEl.textContent = itemValue
+        newEl.style.cursor = 'pointer'
+        newEl.addEventListener("dblclick", function() {
+            let exactLocationOfBooksInDB = ref(database, "books/" + itemId)
+            remove(exactLocationOfBooksInDB)
+
+                
+            
+                // Once removed from the database, re-render the items
+                console.log("Item removed from the database:", itemId);
     
-    shopingEl.innerHTML = listItems
+
+        })
+        
+        shopingEl.append(newEl)
+        
+    }
 }
